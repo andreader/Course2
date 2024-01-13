bb46ebd8bb0dcff6548cc4e5c2f896a2881bba96
Андрей <79538233369@ya.ru>
2024-01-13 13:30:35 +0500

    Master

---
+++ src/pro/sky/JavaCore/PersonService.java
@@ -0,0 +1,9 @@
+package JavaCore;
+
+public class PersonService {
+    public void changePerson(Person person) {
+        person.setName("Ilya");
+        person.setSurname("Lagutenko");
+    }
+
+    }
